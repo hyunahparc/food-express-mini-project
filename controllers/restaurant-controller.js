bae798de6e0e581491024fadeed735aa0d77fab5
hyunahparc <hyunah.parc@gmail.com>
2025-10-23 20:00:58 +0200

    feat: add pagination to Restaurant getall

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -14,27 +14,40 @@ const create = async (req, res) => {
     }
 };
 
-// READ (sorting도)
+// READ
 // GETALL (Public) - 모든 레스토랑 가져오기
-// sorting (정렬) 포함 - name, address
+// sorting (정렬) 포함 - name, address 기준
+// pagination - limit 10
 const getAll = async (req, res) => {
     try {
-        const { sortBy } = req.query;
-        let sortField = "name";
+        const { sortBy, page = 1, limit = 10 } = req.query; // 디폴트로 1페이지, 10개씩 보여짐
+        let sortField = "name"; // 디폴트 정렬 : name
         if(sortBy === "address") sortField = "address";
+ 
+        // 페이지네이션 계산
+        const skip = (page - 1) * limit;
 
-        // find() 후 sort() 적용
-        const restaurants = await Restaurant.find().sort({ [ sortField]: 1 }); // 1 = 오름차순
+        // find() 후 sort() 적용 후 pagination 적용
+        const restaurants = await Restaurant.find()
+            .sort({ [ sortField]: 1 }) // 1 = 오름차순
+            .skip(skip) // 앞에 n개 건너뛰기
+            .limit(parseInt(limit)); // 문자열을 숫자로 변환
+
+        // 전체 개수 (페이지 수 계산용)
+        const total = await Restaurant.countDocuments(); // 전체 레스토랑 개수
+        const totalPages = Math.ceil(total / limit);
+
         res.status(200).json({
-            message: `Restaurant sorted by ${sortField}`,    
+            message: `Restaurant sorted by ${sortField}`,
+            currentPage: parseInt(page),
+            totalPages,
+            totalRestaurants: total,   
             data: restaurants,
         });
     } catch(err) {
         res.status(500).json({ message: err.message });
     }
 };
-
-
 
 // UPDATE (Admin만)
 const update = async (req, res) => {
